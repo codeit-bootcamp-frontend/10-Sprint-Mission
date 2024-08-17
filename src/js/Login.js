04c4c80c8417fcddaeeb584ee16f508014754a2c
김창희 <dpfwlfhtep584@gmail.com>
2024-08-17 23:09:01 +0900

    아이콘 추가, JS 코드 작성, 페이지 작성
    1. Login.html 눈 아이콘 추가, 아이콘 토글 관련 기능 JS 코드 작성
    2. signup 페이지 관련 html 파일들 구현 및 JS 코드 작성

--- src/js/Login.js
+++ src/js/Login.js
@@ -1,18 +1,24 @@
 const loadEvent = new Event('load');
 window.addEventListener('load', () => {
-  console.log('이벤트 발생');
   const btnStatus = document.querySelector('.login-submit-btn');
   const emailInput = document.querySelector('.email-tag');
   const passwordInput = document.querySelector('.password-tag');
+  const passwordIcon = document.querySelector('.password-icon');
 
   const checkInputs = () => {
-    console.log('인풋이벤트');
     if (emailInput.value.trim() !== '' && passwordInput.value.trim() !== '') btnStatus.disabled = false;
     else btnStatus.disabled = true;
   }
 
+  const togglePasswordIcon = () => {
+    const type = passwordInput.type == 'password' ? 'text' : 'password';
+    passwordInput.type = type;
+    passwordIcon.src = type == 'password' ? '/src/assets/img/btn_visibility_off.svg' : '/src/assets/img/btn_visibility_on.svg';
+  }
+
   emailInput.addEventListener('input', checkInputs);
   passwordInput.addEventListener('input', checkInputs);
+  passwordIcon.addEventListener('click', togglePasswordIcon);
 })
 
 window.dispatchEvent(loadEvent);

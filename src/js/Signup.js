f3b04510c592cc1b15a00c5ce96459252b8e2d0c
김창희 <dpfwlfhtep584@gmail.com>
2024-08-20 19:54:55 +0900

    refactor: 로그인 회원가입 페이지 버튼 기능 수정
    - 버튼 disabled 속성 제거, input 값에 따른 버튼 속성 변경 관련 이벤트 리스너와 스크립트 제거

--- src/js/Signup.js
+++ src/js/Signup.js
@@ -1,25 +1,9 @@
 const loadEvent = new Event('load');
 window.addEventListener('load', () => {
-  const btnStatus = document.querySelector('.login-submit-btn');
-  const emailInput = document.querySelector('.email-tag');
   const passwordInput = document.querySelector('.password-tag');
   const passwordCheckInput = document.querySelector('.password-check-tag');
-  const nicknameInput = document.querySelector('.nickname-tag');
   const passwordIcon = document.querySelector('.password-icon');
   const passwordCheckIcon = document.querySelector('.password-check-icon');
-
-  const checkInputs = () => {
-    if (emailInput.value.trim() !== '' && 
-        passwordInput.value.trim() !== '' &&
-        passwordCheckInput.value.trim() !== '' &&
-        nicknameInput.value.trim() !== '' &&
-        (passwordInput.value.trim() == passwordCheckInput.value.trim())
-      ) {
-        btnStatus.disabled = false;
-      } else {
-        btnStatus.disabled = true;
-      }
-  }
 
   const togglePasswordIcon = () => {
     const type = passwordInput.type == 'password' ? 'text' : 'password';
@@ -33,14 +17,8 @@ window.addEventListener('load', () => {
     passwordCheckIcon.src = type == 'password' ? '/src/assets/img/btn_visibility_off.svg' : '/src/assets/img/btn_visibility_on.svg';
   }
 
-  emailInput.addEventListener('input', checkInputs);
-  passwordInput.addEventListener('input', checkInputs);
-  passwordCheckInput.addEventListener('input', checkInputs);
-  nicknameInput.addEventListener('input', checkInputs);
   passwordIcon.addEventListener('click', togglePasswordIcon);
   passwordCheckIcon.addEventListener('click', togglePasswordCheckIcon);
-
-
 })
 
 window.dispatchEvent(loadEvent);

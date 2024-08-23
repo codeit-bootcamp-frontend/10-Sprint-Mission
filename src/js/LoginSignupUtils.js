94ad89c9497acdcc71aba311e34ac4756d717231
김창희 <dpfwlfhtep584@gmail.com>
2024-08-23 10:35:09 +0900

    chore: LoginSignupUtils.js 파일 JSDoc 추가

--- src/js/LoginSignupUtils.js
+++ src/js/LoginSignupUtils.js
@@ -1,3 +1,8 @@
+/**
+ * passwordInput의 type을 text와 password로 toggle하는 함수
+ * @param {input} passwordInput 
+ * @param {span} passwordIcon 
+ */
 export const togglePasswordIcon = (passwordInput, passwordIcon) => {
   const type = passwordInput.type == 'password' ? 'text' : 'password';
   passwordInput.type = type;
@@ -8,6 +13,11 @@ export const togglePasswordIcon = (passwordInput, passwordIcon) => {
       "url('/src/assets/img/btn_visibility_on.svg')";
 }
 
+/**
+ * passwordCheckInput의 type을 text와 password로 toggle하는 함수
+ * @param {input} passwordCheckInput 
+ * @param {span} passwordCheckIcon 
+ */
 export const togglePasswordCheckIcon = (passwordCheckInput, passwordCheckIcon) => {
   const type = passwordCheckInput.type == 'password' ? 'text' : 'password';
   passwordCheckInput.type = type;
@@ -18,6 +28,11 @@ export const togglePasswordCheckIcon = (passwordCheckInput, passwordCheckIcon) =
       "url('/src/assets/img/btn_visibility_on.svg')";
 }
 
+/**
+ * emailInput의 value가 비어있거나 @를 포함하지 않을 때 에러메시지를 보여주는 함수
+ * @param {input} emailInput 
+ * @param {p} emailErrorMessage 
+ */
 export const showEmailErrorMessage = (emailInput, emailErrorMessage) => {
   if (emailInput.value == '') {
     emailErrorMessage.textContent = '이메일을 입력해주세요.';
@@ -28,6 +43,11 @@ export const showEmailErrorMessage = (emailInput, emailErrorMessage) => {
   }
 }
 
+/**
+ * nicknameInput의 value가 비어있을 때 에러메시지를 보여주는 함수
+ * @param {input} nicknameInput 
+ * @param {p} nicknameErrorMessage 
+ */
 export const showNicknameErrorMessage = (nicknameInput, nicknameErrorMessage) => {
   if (nicknameInput.value == '') {
     nicknameErrorMessage.textContent = '닉네임을 입력해주세요.';
@@ -35,6 +55,11 @@ export const showNicknameErrorMessage = (nicknameInput, nicknameErrorMessage) =>
   }
 }
 
+/**
+ * passwordInput의 value가 비어있거나 8자 미만일 때 에러메시지를 보여주는 함수
+ * @param {input} passwordInput 
+ * @param {p} passwordErrorMessage 
+ */
 export const showPasswordErrorMessage = (passwordInput, passwordErrorMessage) => {
   if (passwordInput.value == '') {
     passwordErrorMessage.textContent = '비밀번호를 입력해주세요.';
@@ -45,6 +70,12 @@ export const showPasswordErrorMessage = (passwordInput, passwordErrorMessage) =>
   }
 }
 
+/** 
+ * passwordCheckInput의 value가 passwordInput의 value와 다를 때 에러메시지를 보여주는 함수
+ * @param {input} passwordInput
+ * @param {input} passwordCheckInput
+ * @param {p} passwordCheckErrorMessage
+ */
 export const showPasswordCheckErrorMessage = (passwordInput, passwordCheckInput, passwordCheckErrorMessage) => {
   if (passwordCheckInput.value !== passwordInput.value) {
     passwordCheckErrorMessage.textContent = '비밀번호가 일치하지 않습니다.';
@@ -52,22 +83,48 @@ export const showPasswordCheckErrorMessage = (passwordInput, passwordCheckInput,
   }
 }
 
+
+/**
+ * emailErrorMessage를 숨기는 함수
+ * @param {p} emailErrorMessage 
+ */
 export const hideEmailErrorMessage = (emailErrorMessage) => {
   emailErrorMessage.style.display = 'none';
 }
 
+/**
+ * nicknameErrorMessage를 숨기는 함수
+ * @param {p} nicknameErrorMessage 
+ */
 export const hideNicknameErrorMessage = (nicknameErrorMessage) => {
   nicknameErrorMessage.style.display = 'none';
 }
 
+/**
+ * passwordErrorMessage를 숨기는 함수
+ * @param {p} passwordErrorMessage 
+ */
 export const hidePasswordErrorMessage = (passwordErrorMessage) => {
   passwordErrorMessage.style.display = 'none';
 }
 
+/**
+ * passwordCheckErrorMessage를 숨기는 함수
+ * @param {p} passwordCheckErrorMessage 
+ */
 export const hidePasswordCheckErrorMessage = (passwordCheckErrorMessage) => {
   passwordCheckErrorMessage.style.display = 'none';
 }
 
+
+/**
+ * input 값들의 유효성을 검사하여 버튼을 클릭했을 때 페이지 이동을 결정하는 함수
+ * @param {input} emailInput
+ * @param {input} passwordInput
+ * @param {p} emailErrorMessage
+ * @param {p} passwordErrorMessage
+ * @returns {boolean} bubblingFlag
+ */
 export const routeToItems = (...args) => {
   const [
     emailInput,
@@ -88,6 +145,18 @@ export const routeToItems = (...args) => {
   return bubblingFlag;
 }
 
+/**
+ * input 값들의 유효성을 검사하여 버튼을 클릭했을 때 페이지 이동을 결정하는 함수
+ * @param {input} emailInput
+ * @param {input} passwordInput
+ * @param {input} nicknameInput
+ * @param {input} passwordCheckInput
+ * @param {p} emailErrorMessage
+ * @param {p} nicknameErrorMessage
+ * @param {p} passwordErrorMessage
+ * @param {p} passwordCheckErrorMessage
+ * @returns {boolean} bubblingFlag
+ */
 export const routeToSignin = (...args) => {
   const [
     emailInput,

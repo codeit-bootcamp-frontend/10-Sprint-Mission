03f191c30da8520a46942646788a8330aa14aab2
김창희 <dpfwlfhtep584@gmail.com>
2024-08-23 10:25:15 +0900

    chore: ComponentLoader.js 파일 JSDoc 추가

--- src/js/ComponentLoader.js
+++ src/js/ComponentLoader.js
@@ -1,5 +1,8 @@
 import {ComponentCheckFunction} from './ComponentCheckFunction.js';
 
+/**
+ * 모든 요소를 가져와서 ComponentCheckFunction 함수를 실행한다.
+ */
 const ComponentLoader = () => {
   const allElements = document.getElementsByTagName('*');
   Array.prototype.forEach.call(allElements, (element) => {

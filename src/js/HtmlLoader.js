f0af555379c6b9d3b2a58f5953a9d707e2a67966
김창희 <dpfwlfhtep584@gmail.com>
2024-08-23 10:27:01 +0900

    chore: HtmlLoader.js 파일 JSDoc 추가

--- src/js/HtmlLoader.js
+++ src/js/HtmlLoader.js
@@ -1,5 +1,9 @@
 import { ComponentCheckFunction } from "./ComponentCheckFunction.js";
 
+/**
+ * 최종 index.html 파일을 로드할 때 실행되며 HTML 파일에 data-include-html 속성이 있으면 그 속성값의 경로의 파일을 가져와서 innerHTML로 대체한다.
+ * 해당 파일을 가져올 때 ComponentCheckFunction 함수를 실행한다.
+ */
 const HtmlLoader = () => {
   const allElements = document.getElementsByTagName('*');
   Array.prototype.forEach.call(allElements, (element) => {

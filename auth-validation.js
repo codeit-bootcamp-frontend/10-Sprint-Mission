91309c013be87f2c2a5c34478a565aa0d5886400
envvoo <eoni090@gmail.com>
2024-08-29 22:31:03 +0900

    feat: 로그인 페이지 이메일 / 비밀번호 유효성 검사 추가

--- auth-validation.js
+++ auth-validation.js
@@ -1,3 +1,4 @@
+const form = document.querySelector(".js-form");
 const input = document.querySelectorAll(".js-input");
 const button = document.querySelector(".js-button");
 const emailError = document.querySelector(".js-email-error");
@@ -34,16 +35,46 @@ function valueDefined(e) {
   }
 }
 
-function incorrectEmail() {
-  // 이메일 형식 오류
+function valueValidation() {
+  input.forEach((el) => {
+    el.name === "email" ? emailCheck(el) : pwCheck(el);
+  });
 }
 
-function incorrectPassword() {
-  // 비밀번호 형식 오류
+function emailCheck(el) {
+  if (!el.value) {
+    el.classList.add("js-error");
+    emailError.textContent = "이메일을 입력해주세요";
+  } else {
+    const pattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
+    if (pattern.test(el.value)) {
+    } else {
+      el.classList.add("invalid", "js-error");
+      emailError.textContent = "잘못된 이메일 형식입니다";
+    }
+  }
+  // 이메일 형식 체크
+}
+
+function pwCheck(el) {
+  if (!el.value) {
+    el.classList.add("js-error");
+    pwError.textContent = "비밀번호를 입력해주세요";
+  } else {
+    if (el.value.length > 8) {
+    } else {
+      el.classList.add("invalid", "js-error");
+      pwError.textContent = "비밀번호를 8자 이상 입력해주세요";
+    }
+  }
+  buttonActivation();
+
+  // 비밀번호 형식 체크
 }
 
 function buttonActivation() {}
 
+// 이벤트 리스너
 input.forEach((el) => {
   el.addEventListener("focusout", valueCheck);
 });
@@ -60,4 +91,7 @@ input.forEach((el) => {
   el.addEventListener("change", valueCheck);
 });
 
-button.addEventListener("click", (e) => e.preventDefault());
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  valueValidation();
+});

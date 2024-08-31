8d125d5e8731db634ae96f0606cdada8437f38bb
jsh1147 <jsh1147@naver.com>
2024-08-31 23:58:45 +0900

    feat: 폼 상호작용 기능 작성
    - 폼 입력의 유효성을 검증하는 이벤트 작성
    - 폼 제출 활성화 판단 함수 작성

--- scripts/login_signup.js
+++ scripts/login_signup.js
@@ -1,11 +1,31 @@
 import { isValidEmail, isValidPassword } from "./utils/validate.js";
 
-// 폼 입력 유효성 판단 기능
+const submit = document.forms[0].lastElementChild;
+submit.disabled = true;
+
+let isEmailValid = false;
+let isNicknameValid = false;
+let isPasswordValid = false;
+let isPasswordConfirmValid = false;
 
 const email = document.forms[0]["email"];
 const nickname = document.forms[0]["nickname"];
 const password = document.forms[0]["password"];
 const passwordConfirm = document.forms[0]["passwordConfirm"];
+
+// 폼 제출 활성화 판단
+
+function updateSubmit() {
+  const isSignup = !!nickname;
+  let isFormValid = isEmailValid && isPasswordValid;
+
+  if (isSignup)
+    isFormValid = isFormValid && isNicknameValid && isPasswordConfirmValid;
+
+  submit.disabled = !isFormValid;
+}
+
+// 폼 입력 유효성 판단 기능
 
 function errorChange(input, errorMessage = "") {
   const error = input.parentElement.lastElementChild;
@@ -21,32 +41,63 @@ function errorChange(input, errorMessage = "") {
 }
 
 email.addEventListener("focusout", () => {
-  if (!email.value) errorChange(email, "이메일을 입력해 주세요.");
-  else if (!isValidEmail(email.value))
+  if (!email.value) {
+    errorChange(email, "이메일을 입력해 주세요.");
+    isEmailValid = false;
+  } else if (!isValidEmail(email.value)) {
     errorChange(email, "잘못된 이메일 형식입니다.");
-  else errorChange(email);
+    isEmailValid = false;
+  } else {
+    errorChange(email);
+    isEmailValid = true;
+  }
+
+  updateSubmit();
 });
 
 nickname?.addEventListener("focusout", () => {
-  if (!nickname.value) errorChange(nickname, "닉네임을 입력해 주세요.");
-  else errorChange(nickname);
+  if (!nickname.value) {
+    errorChange(nickname, "닉네임을 입력해 주세요.");
+    isNicknameValid = false;
+  } else {
+    errorChange(nickname);
+    isNicknameValid = true;
+  }
+
+  updateSubmit();
 });
 
 password.addEventListener("focusout", () => {
-  if (!password.value) errorChange(password, "비밀번호를 입력해 주세요.");
-  else if (!isValidPassword(password.value))
+  if (!password.value) {
+    errorChange(password, "비밀번호를 입력해 주세요.");
+    isPasswordValid = false;
+  } else if (!isValidPassword(password.value)) {
     errorChange(password, "비밀번호를 8자 이상 입력해 주세요.");
-  else errorChange(password);
+    isPasswordValid = false;
+  } else {
+    errorChange(password);
+    isPasswordValid = true;
+  }
+
+  updateSubmit();
 });
 
-passwordConfirm?.addEventListener("focusout", () => {
-  if (!passwordConfirm.value)
+passwordConfirm?.addEventListener("input", () => {
+  if (!passwordConfirm.value) {
     errorChange(passwordConfirm, "비밀번호를 입력해 주세요.");
-  else if (!isValidPassword(password.value))
+    isPasswordConfirmValid = false;
+  } else if (!isValidPassword(passwordConfirm.value)) {
     errorChange(passwordConfirm, "비밀번호를 8자 이상 입력해 주세요.");
-  else if (passwordConfirm.value !== password.value)
+    isPasswordConfirmValid = false;
+  } else if (passwordConfirm.value !== password.value) {
     errorChange(passwordConfirm, "비밀번호가 일치하지 않습니다.");
-  else errorChange(passwordConfirm);
+    isPasswordConfirmValid = false;
+  } else {
+    errorChange(passwordConfirm);
+    isPasswordConfirmValid = true;
+  }
+
+  updateSubmit();
 });
 
 // 비밀번호 토글 기능

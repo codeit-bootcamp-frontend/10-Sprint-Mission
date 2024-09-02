4bf38de601edcb056607bf10133d7d02df10cd58
JsH <81379968+jsh1147@users.noreply.github.com>
2024-09-02 18:33:02 +0900

    [정성현] sprint4 (#72)
    * refactor: 스프린트 미션 3주차 피드백 적용
    
    - min-width CSS 속성으로 화면 최소 너비 적용
    - <br> 태그를 통해 줄바꿈 제어
    - 주어진 피그마 디자인에 맞도록 일부 CSS 수정
    
    * feat: 로그인/회원가입 페이지 핸들링 틀 적용
    
    - scripts{ utils } 형태의 JS 파일 구조 생성
    - 페이지 이벤트 / 값 검증 용도의 JS 파일 생성 및 연결
    - 로그인/회원가입 페이지에 핸들링용 요소 추가
    
    * refactor: 폼 요소 id, name 일관되게 변경
    
    - id는 어떤 요소인지를 나타내는 카멜 케이스로 작명
    - name은 담을 내용을 나타내는 카멜 케이스로 작명
    
    * feat: 폼 요소 검증 함수 작성
    
    - 유효 이메일 여부를 반환하는 함수와,
      유효 비밀번호 여부를 반환하는 함수 작성
    
    * feat: 비밀번호 토글 기능 작성
    
    - 비밀번호 input에 부여된 눈 모양 아이콘 클릭 시
      비밀번호 표시 여부와 눈 아이콘 사선 여부가 변경됨
    
    * feat: 로그인/회원가입 폼 입력 유효성 판단 기능 작성
    
    - 입력 요소에서 포커스 아웃 발생 시 유효성 판단
    - 판단 결과에 따라 입력 오류 클래스 및 메시지 적용
    - 오류를 적용시키는 코드를 errorChange 함수로 분리
    
    * feat: 폼 상호작용 기능 작성
    
    - 폼 입력의 유효성을 검증하는 이벤트 작성
    - 폼 제출 활성화 판단 함수 작성
    
    * refactor: 로그인/회원가입 JS 코드 정리
    
    - 코드 최적화 진행
    - 변수 이동, 변수 삭제, 조건식 bool화
    
    * feat: 로그인/회원가입 폼 제출 대응 코드 작성
    
    - 폼 제출 시 기본 행동 방지 후 목표 페이지로 이동
    - 목표 페이지 HTML 코드 작성
    
    * refactor: 로그인/회원가입 페이지 CSS 수정
    
    - 에러 <span> 요소 추가를 고려한 CSS 수정 진행
    
    * docs: README.md 파일 추가 작성
    
    - 대표 이미지 삽입
    - 기술 스택 수정
    - 네이밍 규칙, 프로젝트 구조 추가
    
    * fix: <label> 잘못된 for 값 수정
    
    - <label>의 for 값이 <input>의 name을 향하던 것을
      <input>의 id를 향하도록 수정

--- README.md
+++ README.md
@@ -6,13 +6,47 @@
 - 코드잇 스프린트 FE-10 스프린트 미션
 - 2024.08.05.(월) ~ 개발 진행 중
 
+![판다마켓 이미지](/images/common/banner_w1440.png)
+
 <br />
 
 ## 기술 스택
 
 - **개발**
-  - HTML, CSS
+  - HTML, CSS, JS
 - **IDE**
   - Visual Studio Code
 - **배포**
   - Netlify
+
+<br />
+
+## 네이밍 규칙
+
+- **HTML**
+  - id: camelCase
+  - name: camelCase
+  - class: camelCase with BEM
+- **CSS**
+  - 변수: kebab-case
+- **JS**
+  - 식별자: camelCase
+
+<br />
+
+## 프로젝트 구조
+
+```
+codeit-FE10-sprint-mission  : 프로젝트 디렉터리
+├─ images                   : 이미지
+│   └─ common               : 공통 이미지
+│   └─ 페이지별 이미지       : 페이지별 이미지
+├─ scripts                  : JS
+│   └─ utils                : 유틸 함수
+│   └─ 페이지별 JS           : 페이지별 JS
+├─ styles                   : CSS
+│   └─ common               : 공용 CSS
+│   └─ 페이지별 CSS          : 페이지별 CSS
+├─ 페이지별 HTML             : 페이지별 HTML
+└─ README.md                : README 문서
+```
---
+++ scripts/login_signup.js
@@ -0,0 +1,134 @@
+import { isValidEmail, isValidPassword } from "./utils/validate.js";
+
+const form = document.forms[0];
+const email = form["email"];
+const nickname = form["nickname"];
+const password = form["password"];
+const passwordConfirm = form["passwordConfirm"];
+const submit = form.lastElementChild;
+
+// 폼 유효성 & 활성화 초기화
+
+let isEmailValid = false;
+let isNicknameValid = false;
+let isPasswordValid = false;
+let isPasswordConfirmValid = false;
+submit.disabled = true;
+
+// 폼 제출 활성화 판단
+
+function updateSubmit() {
+  let isFormValid = isEmailValid && isPasswordValid;
+
+  if (!!nickname)
+    isFormValid = isFormValid && isNicknameValid && isPasswordConfirmValid;
+
+  submit.disabled = !isFormValid;
+}
+
+// 폼 제출 이벤트 변경
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!!nickname) document.location.href = "/signin.html";
+  else document.location.href = "/items.html";
+});
+
+// 폼 입력 유효성 판단
+
+function errorChange(input, errorMessage = "") {
+  const error = input.parentElement.lastElementChild;
+
+  if (!!errorMessage) {
+    input.classList.add("formField__input--error");
+    error.classList.add("formField__error--active");
+  } else {
+    input.classList.remove("formField__input--error");
+    error.classList.remove("formField__error--active");
+  }
+  error.textContent = errorMessage;
+}
+
+email.addEventListener("focusout", () => {
+  if (!email.value) {
+    errorChange(email, "이메일을 입력해 주세요.");
+    isEmailValid = false;
+  } else if (!isValidEmail(email.value)) {
+    errorChange(email, "잘못된 이메일 형식입니다.");
+    isEmailValid = false;
+  } else {
+    errorChange(email);
+    isEmailValid = true;
+  }
+
+  updateSubmit();
+});
+
+nickname?.addEventListener("focusout", () => {
+  if (!nickname.value) {
+    errorChange(nickname, "닉네임을 입력해 주세요.");
+    isNicknameValid = false;
+  } else {
+    errorChange(nickname);
+    isNicknameValid = true;
+  }
+
+  updateSubmit();
+});
+
+password.addEventListener("focusout", () => {
+  if (!password.value) {
+    errorChange(password, "비밀번호를 입력해 주세요.");
+    isPasswordValid = false;
+  } else if (!isValidPassword(password.value)) {
+    errorChange(password, "비밀번호를 8자 이상 입력해 주세요.");
+    isPasswordValid = false;
+  } else {
+    errorChange(password);
+    isPasswordValid = true;
+  }
+
+  updateSubmit();
+});
+
+passwordConfirm?.addEventListener("focusout", () => {
+  if (!isPasswordValid) {
+    errorChange(passwordConfirm, "먼저 위에 적절한 비밀번호를 입력해 주세요.");
+    isPasswordConfirmValid = false;
+  } else if (!passwordConfirm.value) {
+    errorChange(passwordConfirm, "비밀번호를 입력해 주세요.");
+    isPasswordConfirmValid = false;
+  } else if (!isValidPassword(passwordConfirm.value)) {
+    errorChange(passwordConfirm, "비밀번호를 8자 이상 입력해 주세요.");
+    isPasswordConfirmValid = false;
+  } else if (passwordConfirm.value !== password.value) {
+    errorChange(passwordConfirm, "비밀번호가 일치하지 않습니다.");
+    isPasswordConfirmValid = false;
+  } else {
+    errorChange(passwordConfirm);
+    isPasswordConfirmValid = true;
+  }
+
+  updateSubmit();
+});
+
+// 비밀번호 표시 전환
+
+const toggles = document.querySelectorAll(".formField__toggleButton");
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    const input = toggle.previousElementSibling;
+    const icon = toggle.firstElementChild;
+
+    if (input.type === "password") {
+      input.type = "text";
+      icon.src = "/images/login_signup/eye_open.svg";
+      icon.alt = "비밀번호 가리기";
+    } else {
+      input.type = "password";
+      icon.src = "/images/login_signup/eye_close.svg";
+      icon.alt = "비밀번호 보기";
+    }
+  });
+});

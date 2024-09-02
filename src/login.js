4259183542cd485443b7b73fef3154f141d2ceac
naji <jitwona99.gmail.com>
2024-09-02 09:47:58 +0900

    style: apply prettier formatting to improve code readability

--- src/login.js
+++ src/login.js
@@ -1,23 +1,35 @@
-import { 
-  validateEmail, 
-  validatePassword, 
+import {
+  validateEmail,
+  validatePassword,
   handleSubmitButton,
-  togglePasswordVisibility 
-} from './auth.js';
+  togglePasswordVisibility,
+} from "./auth.js";
 
-const $emailInput = document.querySelector('#email');
-const $emailContainer = document.querySelector('.email-container');
-const $emailValidationMessage = $emailContainer.querySelector('.validation-message');
+const $emailInput = document.querySelector("#email");
+const $emailContainer = document.querySelector(".email-container");
+const $emailValidationMessage = $emailContainer.querySelector(
+  ".validation-message"
+);
 
-const $passwordInput = document.querySelector('#password');
-const $passwordContainer = document.querySelector('.password-container');
-const $passwordValidationMessage = $passwordContainer.querySelector('.validation-message');
+const $passwordInput = document.querySelector("#password");
+const $passwordContainer = document.querySelector(".password-container");
+const $passwordValidationMessage = $passwordContainer.querySelector(
+  ".validation-message"
+);
 
-const $submitButton = document.querySelector('.auth-form > button');
-const $passwordVisibility = document.querySelector('.btn-eye');
+const $submitButton = document.querySelector(".auth-form > button");
+const $passwordVisibility = document.querySelector(".btn-eye");
 
-$emailInput.addEventListener('focusout', () => validateEmail($emailInput, $emailValidationMessage));
-$passwordInput.addEventListener('focusout', () => validatePassword($passwordInput, $passwordValidationMessage));
+$emailInput.addEventListener("focusout", () =>
+  validateEmail($emailInput, $emailValidationMessage)
+);
 
-$submitButton.addEventListener('click', (event) => handleSubmitButton(event, './items'));
-$passwordVisibility.addEventListener('click', togglePasswordVisibility);
+$passwordInput.addEventListener("focusout", () =>
+  validatePassword($passwordInput, $passwordValidationMessage)
+);
+
+$submitButton.addEventListener("click", (event) =>
+  handleSubmitButton(event, "./items")
+);
+
+$passwordVisibility.addEventListener("click", togglePasswordVisibility);

45d76f5321d1bc133fa604aa83db986dfe8366d2
heejin <dlwlrgkwk82@gmail.com>
2024-09-04 16:40:25 +0900

    feat: use outlet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './pages/Root';
 import Items from './pages/Items';
+import Home from './pages/Home';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>index</div>,
-  },
-  {
-    path: '/items',
-    element: <Items />,
+    element: <Root />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'items', element: <Items /> },
+    ],
   },
 ]);
 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <nav>
+      <ul>
+        <li>자유게시판</li>
+        <li>중고마켓</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;

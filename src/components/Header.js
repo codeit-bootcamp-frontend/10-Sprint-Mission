6743d6ea21b14a8630f914f41d049cfd2e8ccf34
heejin <dlwlrgkwk82@gmail.com>
2024-09-04 17:14:30 +0900

    feat: add header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,28 @@
+import { useNavigate } from 'react-router-dom';
+import logo from '../assets/panda_logo.svg';
+import avatar from '../assets/avatar.svg';
+import './Header.css';
+
 function Header() {
+  const navigate = useNavigate();
+
+  const handleImageClick = () => {
+    navigate('/');
+  };
+
   return (
-    <nav>
-      <ul>
-        <li>자유게시판</li>
-        <li>중고마켓</li>
-      </ul>
-    </nav>
+    <header className="Header">
+      <div className="header-container max-container">
+        <img src={logo} alt="판다마켓 로고" onClick={handleImageClick} />
+        <nav className="nav">
+          <ul className="nav-menu">
+            <li>자유게시판</li>
+            <li>중고마켓</li>
+          </ul>
+        </nav>
+        <img src={avatar} alt="아바타" />
+      </div>
+    </header>
   );
 }
 

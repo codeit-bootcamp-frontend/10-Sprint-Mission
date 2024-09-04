d1c61879c7e72563efd6df41938e1f52ef819fe5
heejin <dlwlrgkwk82@gmail.com>
2024-09-05 01:29:17 +0900

    feat: add search select box

--- src/components/SortDropdown.js
+++ src/components/SortDropdown.js
@@ -1,5 +1,21 @@
 import './SortDropdown.css';
+import arrowIcon from '../assets/ic_arrow_down.svg';
 
-function SortDropdown() {}
+function SortDropdown() {
+  return (
+    <div className="SortDropdown">
+      <div className="SortDropdown-selected">
+        <span>최신순</span>
+        <img src={arrowIcon} alt="아래방향 화살표" />
+      </div>
+      <div className="SortDropdown-options-container">
+        <ul className="SortDropdown-options">
+          <li>최신순</li>
+          <li>좋아요순</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 export default SortDropdown;

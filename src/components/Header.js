d733abcdccb2f4715ad8ffc8ed8b1873c964c3e8
naji <jitwona99.gmail.com>
2024-09-07 17:04:41 +0900

    feat: implement Header component for site navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,37 @@
+import { Link, NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
+import pandaTypoImg from "../assets/panda_typo.svg";
+import profileImg from "../assets/ic_profile.svg";
+
+function getLinkStyle({ isActive }) {
+  return isActive ? styles.active : "";
+}
 
 const Header = () => {
-  return <header className={styles.header}>Header</header>;
+  return (
+    <header className={styles.header}>
+      <ul className={styles.menu}>
+        <li>
+          <Link to="/">
+            <img
+              src={pandaTypoImg}
+              className={styles.logo}
+              alt="판다마켓 로고"
+            />
+          </Link>
+        </li>
+        <li>자유게시판</li>
+        <li>
+          <NavLink to="/items" className={getLinkStyle}>
+            중고마켓
+          </NavLink>
+        </li>
+      </ul>
+      <Link to="/login">
+        <img src={profileImg} className={styles.profile} alt="프로필" />
+      </Link>
+    </header>
+  );
 };
 
 export default Header;
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
+import HomePage from "../pages/HomePage";
 import ProductListPage from "../pages/ProductListPage";
 import AddProductPage from "../pages/AddProductPage";
 
@@ -8,6 +9,7 @@ const AppRoutes = () => {
     <Router>
       <Routes>
         <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
           <Route path="items" element={<ProductListPage />} />
           <Route path="additem" element={<AddProductPage />} />
         </Route>

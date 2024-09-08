19be5e72915cd430856d561505a6bd2898545aec
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-08 14:14:10 +0900

    feat: pages 디렉토리 생성 및 items 작성

---
+++ src/pages/items/Items.jsx
@@ -0,0 +1,15 @@
+import Header from "components/Header";
+import AllProducts from "components/items/AllProducts";
+import BestProducts from "components/items/BestProducts";
+
+const Items = () => {
+  return (
+    <div>
+      <Header/>
+      <BestProducts/>
+      <AllProducts/>
+    </div>
+  );
+}
+
+export default Items;

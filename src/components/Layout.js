9a4d4669b52fa2a6196e0183b9dc5793a8b57be0
jsh1147 <jsh1147@naver.com>
2024-09-09 19:48:23 +0900

    feat: 헤더 nav 조건부 디자인 작성
    - 현재 페이지에 해당하는 nav 아이템에 색상 적용
    - 추가로, main 요소를 페이지 컴포넌트에서 다루도록 제거

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,28 +1,32 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 import profileImage from "../assets/profile_image_w40x3.png";
 import "./Layout.css";
 
 export default function Layout() {
+  const { pathname } = useLocation();
+
+  const checkPath = (path) => {
+    return pathname === path ? "header__navItem--active" : undefined;
+  };
+
   return (
     <>
       <header className="header">
         <Link className="header__logo" to="/" aria-label="판다마켓 바로가기" />
         <nav className="header__nav">
           <ul className="header__navList">
-            <li>
-              <Link to="/">자유게시판</Link>
+            <li className={checkPath("/boards")}>
+              <Link to="/boards">자유게시판</Link>
             </li>
-            <li>
+            <li className={checkPath("/items")}>
               <Link to="/items">중고마켓</Link>
             </li>
           </ul>
         </nav>
         <img className="header__profile" src={profileImage} alt="프로필 사진" />
       </header>
-      <main className="main">
-        <Outlet />
-      </main>
+      <Outlet />
     </>
   );
 }

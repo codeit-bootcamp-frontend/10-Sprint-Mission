7232780332d4e174a6c16a3affda173ab85bd06c
Suzy-Lee <>
2024-09-10 16:14:52 +0900

    feat: Card 컴포넌트 추가

---
+++ src/components/Card.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+import likeButton from '../assets/images/ic_heart.svg';
+import styles from './Card.module.css';
+
+function Card({ image, title, price, like }) {
+  return (
+    <div className={styles.container}>
+      <figure className={styles.productImg}>
+        <img src={image} alt={title} />
+      </figure>
+      <div>
+        <p className={styles.infoTitle}>{title}</p>
+        <p className={styles.infoPrice}>{price}원</p>
+        <div className={styles.infoLike}>
+          <img src={likeButton} alt='좋아요 아이콘' />
+          <span>{like}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;

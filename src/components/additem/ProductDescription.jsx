450d9969ff1fd0f5a3af2e36c2da1415fbf87acb
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-12 22:35:16 +0900

    feat: ProductDescription JSX CSS 작성 및 폰트패밀리 미적용 에러 수정

--- src/components/additem/ProductDescription.jsx
+++ src/components/additem/ProductDescription.jsx
@@ -1,7 +1,11 @@
+import styles from "./ProductDescription.module.css";
+import commonStyles from "./AddItemCommon.module.css";
+
 const ProductDescription = () => {
   return (
-    <div>
-      
+    <div className={commonStyles['common-container']}>
+      <h5 className={commonStyles['common-title']}>상품 설명</h5>
+      <textarea className={styles['product-description']} placeholder="상품 설명을 입력해주세요"/>
     </div>
   );
 };

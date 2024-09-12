2cbe9105ddebe51ea0518e5b871a2d9f2295a9af
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-12 20:11:49 +0900

    feat: AddProductImage jsx 작성

--- src/components/additem/AddProductImage.jsx
+++ src/components/additem/AddProductImage.jsx
@@ -1,7 +1,16 @@
+import styles from "./AddProductImage.module.css";
+import commonStyles from './AddItemCommon.module.css';
+
 const AddProductImage = () => {
   return (
-    <div>
-      
+    <div className={`${commonStyles['common-container']} ${styles['container']}`}>
+      <h5 className={commonStyles['common-product-title']}>상품 이미지</h5>
+      <div className={styles['image-container']}>
+        <div className={styles['image-upload']}>
+          <input type="file" id="file" accept="image/*" />
+        </div>
+        <img className={styles['image-preview']} src="" alt=""/>
+      </div>
     </div>
   );
 };

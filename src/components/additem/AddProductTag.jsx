38d000d7820163b0b3d8fa771452a4eb6deaefc3
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-12 22:50:42 +0900

    feat: AddProductTag JSX CSS 작성

--- src/components/additem/AddProductTag.jsx
+++ src/components/additem/AddProductTag.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import syltes from './AddProductTag.module.css';
+import commonStyles from './AddItemCommon.module.css';
 
 const AddProductTag = () => {
   return (
-    <div>
-      
+    <div className={commonStyles['common-container']}>
+      <h5 className={commonStyles['common-product-title']}>태그</h5>
+      <input type="text" className={syltes['tag-input']} placeholder="태그를 입력해주세요" />
     </div>
   );
 };

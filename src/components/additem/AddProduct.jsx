00c8f01857809d29fd0aebe3ad34789f6204a438
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-12 19:36:53 +0900

    feat: AddProdcut css 작성

--- src/components/additem/AddProduct.jsx
+++ src/components/additem/AddProduct.jsx
@@ -5,7 +5,7 @@ const AddProduct = () => {
   return (
     <div className={`${commonStyles['common-container']} ${styles['container']}`}>
       <h4 className={styles['add-product-title']}>상품 등록하기</h4>
-      <button className={styles['add-button']} type="button">등록</button>
+      <button className={styles['add-button']} type="button" disabled>등록</button>
     </div>
   );
 };

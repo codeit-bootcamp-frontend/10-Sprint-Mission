dc15d77ebfd41e35d7b344eb488ac4143f0dae11
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-12 22:47:18 +0900

    feat: ProductPrice JSX CSS 작성

--- src/components/additem/ProductDescription.jsx
+++ src/components/additem/ProductDescription.jsx
@@ -4,7 +4,7 @@ import commonStyles from "./AddItemCommon.module.css";
 const ProductDescription = () => {
   return (
     <div className={commonStyles['common-container']}>
-      <h5 className={commonStyles['common-title']}>상품 설명</h5>
+      <h5 className={commonStyles['common-product-title']}>상품 설명</h5>
       <textarea className={styles['product-description']} placeholder="상품 설명을 입력해주세요"/>
     </div>
   );
--- src/components/additem/ProductPrice.jsx
+++ src/components/additem/ProductPrice.jsx
@@ -1,7 +1,11 @@
+import styles from "./ProductPrice.module.css";
+import commonStyles from "./AddItemCommon.module.css";
+
 const ProductPrice = () => {
   return (
-    <div>
-      
+    <div className={commonStyles['common-container']}>
+      <h5 className={commonStyles['common-product-title']}>판매가격</h5>
+      <input type="number" className={styles['price-input']} placeholder="판매 가격을 입력해주세요" />
     </div>
   );
 };

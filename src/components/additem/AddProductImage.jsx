4d14483f0ac496870aa4bc860185b4a6f91615ca
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-12 21:06:40 +0900

    chore: AddProductImage css 일부 수정

--- src/components/additem/AddProductImage.jsx
+++ src/components/additem/AddProductImage.jsx
@@ -1,10 +1,10 @@
 import styles from "./AddProductImage.module.css";
-import commonStyles from './AddItemCommon.module.css';
+import commonStyles from "./AddItemCommon.module.css";
 import {ReactComponent as PlusIcon} from 'assets/imgs/ic_plus.svg';
 
 const AddProductImage = () => {
   return (
-    <div className={commonStyles['common-container']}>
+    <div className={`${commonStyles['common-container']} ${styles['container']}`}>
       <h5 className={commonStyles['common-product-title']}>상품 이미지</h5>
       <div className={styles['image-container']}>
         <div className={styles['image-upload']}>
@@ -15,6 +15,7 @@ const AddProductImage = () => {
             </div>
           </label>
           <input className={styles['image-upload-input']} type="file" id="file" accept="image/*" />
+          
         </div>
         <img className={styles['image-preview']} src="" alt=""/>
       </div>

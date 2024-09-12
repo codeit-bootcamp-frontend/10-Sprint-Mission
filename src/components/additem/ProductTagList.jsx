00c4a84d67f1012184899177067c048305880dcb
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-12 23:51:27 +0900

    feat: ProductTagList JSX CSS 작성

--- src/components/additem/ProductTagList.jsx
+++ src/components/additem/ProductTagList.jsx
@@ -1,7 +1,22 @@
-const ProductTagList = () => {
+import { useState } from 'react';
+import styles from './ProductTagList.module.css';
+import {ReactComponent as DeleteIcon} from 'assets/imgs/ic_delete.svg';
+
+const ProductTagList = ({taglist = []}) => {
+  const [tags, setTags] = useState(taglist);
+
+  const deleteTag = (tag) => {
+    setTags(tags.filter((e) => e !== tag));
+  }
+
   return (
-    <div>
-      
+    <div className={styles['container']}>
+      {tags && tags.map((tag, index) => (
+        <div className={styles['tag']} key={index}>
+          <span>#{tag}</span>
+          <DeleteIcon onClick={()=>deleteTag(tag)}/>
+        </div>
+      ))}
     </div>
   );
 };

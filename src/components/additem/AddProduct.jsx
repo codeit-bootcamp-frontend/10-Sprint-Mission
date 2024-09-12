29b15c700a424c0796a7dc9dba4db355b797b8d9
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-12 21:12:48 +0900

    chore: additem css 수정

--- src/components/additem/AddProduct.jsx
+++ src/components/additem/AddProduct.jsx
@@ -1,9 +1,8 @@
 import styles from "./AddProduct.module.css";
-import commonStyles from './AddItemCommon.module.css';
 
 const AddProduct = () => {
   return (
-    <div className={`${commonStyles['common-container']} ${styles['container']}`}>
+    <div className={styles['container']}>
       <h4 className={styles['add-product-title']}>상품 등록하기</h4>
       <button className={styles['add-button']} type="button" disabled>등록</button>
     </div>

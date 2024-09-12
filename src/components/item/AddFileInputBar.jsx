8a1570dfd07cf9f996f62cf2faa03ac2cb009951
heejin <dlwlrgkwk82@gmail.com>
2024-09-12 14:41:25 +0900

    feat: add FileInput responsive css

--- src/components/item/AddFileInputBar.jsx
+++ src/components/item/AddFileInputBar.jsx
@@ -42,30 +42,32 @@ function AddFileInputBar({ name, value, onChange }) {
 
   return (
     <section className="AddFileInputBar">
-      <div onClick={handleOnClick}>
-        <label htmlFor="file">
-          <div className="btn-upload">
-            <img src={plusIcon} alt="플러스 아이콘" />
-            <span className="btn-upload-description">이미지 등록</span>
-          </div>
-        </label>
-      </div>
-      <input
-        type="file"
-        name={name}
-        id="file"
-        accept="image/png, image/jpeg"
-        ref={inputRef}
-        onChange={handleChange}
-      />
-      {preview && (
-        <div className="preview-wrapper">
-          <img src={preview} alt="이미지 미리보기" className="preview" />
-          <button className="btn-file-clear" onClick={handleClearClick}>
-            <img src={deleteIcon} alt="삭제 아이콘" />
-          </button>
+      <div className="image-container">
+        <div onClick={handleOnClick}>
+          <label htmlFor="file">
+            <div className="btn-upload">
+              <img src={plusIcon} alt="플러스 아이콘" />
+              <span className="btn-upload-description">이미지 등록</span>
+            </div>
+          </label>
         </div>
-      )}
+        <input
+          type="file"
+          name={name}
+          id="file"
+          accept="image/png, image/jpeg"
+          ref={inputRef}
+          onChange={handleChange}
+        />
+        {preview && (
+          <div className="preview-wrapper">
+            <img src={preview} alt="이미지 미리보기" className="preview" />
+            <button className="btn-file-clear" onClick={handleClearClick}>
+              <img src={deleteIcon} alt="삭제 아이콘" />
+            </button>
+          </div>
+        )}
+      </div>
       {error && <span className="file-upload-error">{error}</span>}
     </section>
   );

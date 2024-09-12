dbedcd4d8e8437704922fa35dcb8409bbea6a3d1
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-12 19:25:05 +0900

    feat: additem comm css 파일 추가 및 container 클래스 작성

--- src/components/additem/AddProduct.jsx
+++ src/components/additem/AddProduct.jsx
@@ -1,6 +1,9 @@
+import styles from "./AddProduct.module.css";
+import commonStyles from './AddItemCommon.module.css';
+
 const AddProduct = () => {
   return (
-    <div>
+    <div className={`${commonStyles['common-container']} ${styles['container']}`}>
       
     </div>
   );

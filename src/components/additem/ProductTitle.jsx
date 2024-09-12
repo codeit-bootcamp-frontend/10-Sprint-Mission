99514a26d777ce014a6151403e9f900c3a697f8f
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-12 22:05:09 +0900

    feat: ProductTitle JSX CSS 작성

--- src/components/additem/ProductTitle.jsx
+++ src/components/additem/ProductTitle.jsx
@@ -1,7 +1,11 @@
+import styles from "./ProductTitle.module.css";
+import commonStyles from "./AddItemCommon.module.css";
+
 const ProductTitle = () => {
   return (
-    <div>
-      
+    <div className={commonStyles['common-container']}>
+      <h5 className={commonStyles['common-product-title']}>상품명</h5>
+      <input className={styles['product-title-input']} type="text" placeholder="상품명을 입력해주세요" />
     </div>
   );
 };

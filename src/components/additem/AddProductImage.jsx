ebfb1e513e74eac44b2949de108c849bc63b414f
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-12 20:45:39 +0900

    feat: AddProductImage css 작성

--- src/components/additem/AddProductImage.jsx
+++ src/components/additem/AddProductImage.jsx
@@ -1,13 +1,20 @@
 import styles from "./AddProductImage.module.css";
 import commonStyles from './AddItemCommon.module.css';
+import {ReactComponent as PlusIcon} from 'assets/imgs/ic_plus.svg';
 
 const AddProductImage = () => {
   return (
-    <div className={`${commonStyles['common-container']} ${styles['container']}`}>
+    <div className={commonStyles['common-container']}>
       <h5 className={commonStyles['common-product-title']}>상품 이미지</h5>
       <div className={styles['image-container']}>
         <div className={styles['image-upload']}>
-          <input type="file" id="file" accept="image/*" />
+          <label htmlFor="file" className={styles['image-upload-label']}>
+            <div className={styles['image-upload-label-content']}>
+              <PlusIcon />
+              <p>이미지 등록</p>
+            </div>
+          </label>
+          <input className={styles['image-upload-input']} type="file" id="file" accept="image/*" />
         </div>
         <img className={styles['image-preview']} src="" alt=""/>
       </div>

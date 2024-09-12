a05748236fc272f34cfd6e5b4ac4a13f63b73669
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-12 21:31:14 +0900

    feat: AddProductImage 상품 이미지 JS 추가

--- src/components/additem/AddProductImage.jsx
+++ src/components/additem/AddProductImage.jsx
@@ -1,8 +1,31 @@
 import styles from "./AddProductImage.module.css";
 import commonStyles from "./AddItemCommon.module.css";
 import {ReactComponent as PlusIcon} from 'assets/imgs/ic_plus.svg';
+import { useEffect } from "react";
 
 const AddProductImage = () => {
+
+  useEffect(() => {
+    const fileInput = document.querySelector('input[type="file"]');
+    const imagePreview = document.querySelector('#image-preview');
+    
+    fileInput.addEventListener('change', (e) => {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      
+      reader.onload = (e) => {
+        imagePreview.src = e.target.result;
+      };
+      
+      reader.readAsDataURL(file);
+    });
+
+    return () => {
+      fileInput.removeEventListener('change', () => {});
+    };
+    
+  }, []);
+
   return (
     <div className={commonStyles['common-container']}>
       <h5 className={commonStyles['common-product-title']}>상품 이미지</h5>
@@ -15,9 +38,8 @@ const AddProductImage = () => {
             </div>
           </label>
           <input className={styles['image-upload-input']} type="file" id="file" accept="image/*" />
-          
         </div>
-        <img className={styles['image-preview']} src="" alt=""/>
+        <img className={styles['image-preview']} id="image-preview" src="" alt=""/>
       </div>
     </div>
   );

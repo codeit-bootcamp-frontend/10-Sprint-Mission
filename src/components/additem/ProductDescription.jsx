f234e326654861d64fdc3a51c6a8edfbc393002e
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-13 13:54:18 +0900

    feat: ProductDescription 커스텀 훅 추가

--- src/components/additem/ProductDescription.jsx
+++ src/components/additem/ProductDescription.jsx
@@ -1,11 +1,18 @@
 import styles from "./ProductDescription.module.css";
 import commonStyles from "./AddItemCommon.module.css";
+import { useAddItemSharedData } from "./useAddItemSharedData";
 
 const ProductDescription = () => {
+  const { productDescription, setProductDescription } = useAddItemSharedData();
+
+  const handleProductDescriptionChange = (e) => {
+    setProductDescription(e.target.value);
+  }
+
   return (
     <div className={commonStyles['common-container']}>
       <h5 className={commonStyles['common-product-title']}>상품 설명</h5>
-      <textarea className={styles['product-description']} placeholder="상품 설명을 입력해주세요"/>
+      <textarea className={styles['product-description']} value={productDescription} onChange={handleProductDescriptionChange} placeholder="상품 설명을 입력해주세요"/>
     </div>
   );
 };

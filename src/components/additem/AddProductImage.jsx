7df4499675586ed05b5dc88bd77aead65d17ea0a
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-13 09:45:05 +0900

    fix: AddProductImage useRef 사용 및 이벤트 함수 분리

--- src/components/additem/AddProductImage.jsx
+++ src/components/additem/AddProductImage.jsx
@@ -2,40 +2,35 @@ import styles from "./AddProductImage.module.css";
 import commonStyles from "./AddItemCommon.module.css";
 import {ReactComponent as PlusIcon} from 'assets/imgs/ic_plus.svg';
 import {ReactComponent as DeleteIcon} from 'assets/imgs/ic_delete.svg';
-import { useEffect } from "react";
+import { useRef } from "react";
 
 const AddProductImage = () => {
+  const imagePreviewRef = useRef(null);
+  const imageContentRef = useRef(null);
 
-  useEffect(() => {
-    const fileInput = document.querySelector('input[type="file"]');
-    const imagePreview = document.querySelector('#image-preview');
-    const imageContent = document.querySelector('#image-content');
+  const handleFileRead = (e) => {
+    imageContentRef.current.style.display = 'block';
 
-    const fileRead = (e) => {
-      imageContent.style.display = 'block';
-      try {
-        const file = e.target.files[0];
-        const reader = new FileReader();
-        
-        reader.onload = (e) => {
-          imagePreview.src = e.target.result;
-        };
-        
-        reader.readAsDataURL(file);
+    try {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      
+      reader.onload = (e) => {
+        imagePreviewRef.current.src = e.target.result;
+      };
+      
+      reader.readAsDataURL(file);
 
-      } catch (error) {
-        console.error(error);
-        alert('이미지를 불러오는데 실패했습니다');
-      }
+    } catch (error) {
+      console.error(error);
+      alert('이미지를 불러오는데 실패했습니다');
     }
-    
-    fileInput.addEventListener('change', fileRead);
+  }
 
-    return () => {
-      fileInput.removeEventListener('change', fileRead);
-    };
-    
-  }, []);
+  const handleDeleteImage = () => {
+    imageContentRef.current.style.display = 'none';
+    imagePreviewRef.current.src = '';
+  }
 
   return (
     <div className={commonStyles['common-container']}>
@@ -48,11 +43,11 @@ const AddProductImage = () => {
               <p>이미지 등록</p>
             </div>
           </label>
-          <input className={styles['image-upload-input']} type="file" id="file" accept="image/*" />
+          <input className={styles['image-upload-input']} onChange={handleFileRead} type="file" id="file" accept="image/*" />
         </div>
-        <div id="image-content" className={styles['image-content']}>
-          <img className={styles['image-preview']} id="image-preview" src="" alt=""/>
-          <DeleteIcon className={styles['delete-icon']} />
+        <div ref={imageContentRef} id="image-content" className={styles['image-content']}>
+          <img ref={imagePreviewRef} className={styles['image-preview']} id="image-preview" src="" alt=""/>
+          <DeleteIcon className={styles['delete-icon']} onClick={handleDeleteImage}/>
         </div>
       </div>
     </div>

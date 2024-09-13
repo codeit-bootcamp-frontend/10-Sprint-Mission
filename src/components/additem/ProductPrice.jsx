d950581b8d8cf8cc829a16fdfb0e370674778371
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-13 12:18:37 +0900

    fix: ProductPrice toLocalString 적용

--- src/components/additem/ProductPrice.jsx
+++ src/components/additem/ProductPrice.jsx
@@ -1,11 +1,29 @@
 import styles from "./ProductPrice.module.css";
 import commonStyles from "./AddItemCommon.module.css";
+import { useState } from "react";
 
 const ProductPrice = () => {
+  const [value, setValue] = useState('');
+
+  const handleChange = (e) => {
+    if(e.target.value === '') {
+      setValue('');
+      return;
+    }
+
+    const inputValue = Number(e.target.value.replace(/[^0-9]/g, '')).toLocaleString();
+    
+    if(inputValue === '') {
+      setValue('');
+      return;
+    }
+    setValue(inputValue);
+  }
+
   return (
     <div className={commonStyles['common-container']}>
       <h5 className={commonStyles['common-product-title']}>판매가격</h5>
-      <input type="number" className={styles['price-input']} placeholder="판매 가격을 입력해주세요" />
+      <input type="text" value={value} onChange={handleChange} className={styles['price-input']} placeholder="판매 가격을 입력해주세요" />
     </div>
   );
 };

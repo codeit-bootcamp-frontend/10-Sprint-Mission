76bea734ad8e4f05302fc83e59774429d86025ba
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-13 14:04:02 +0900

    feat: AddProductTag 커스텀 훅 적용

--- src/components/additem/AddProductTag.jsx
+++ src/components/additem/AddProductTag.jsx
@@ -1,11 +1,26 @@
 import syltes from './AddProductTag.module.css';
 import commonStyles from './AddItemCommon.module.css';
+import { useAddItemSharedData } from './useAddItemSharedData';
 
 const AddProductTag = () => {
+  const { productTags, setProductTags } = useAddItemSharedData();
+
+  const handleKeyDown = (e) => {
+    if(e.key === 'Enter') {
+      const tag = e.target.value;
+      if(tag.trim() === '') {
+        return;
+      }
+
+      setProductTags([...productTags, `#${tag}`]);
+      e.target.value = '';
+    }
+  }
+
   return (
     <div className={commonStyles['common-container']}>
       <h5 className={commonStyles['common-product-title']}>태그</h5>
-      <input type="text" className={syltes['tag-input']} placeholder="태그를 입력해주세요" />
+      <input type="text" className={syltes['tag-input']} onKeyDown={handleKeyDown} placeholder="태그를 입력해주세요" />
     </div>
   );
 };

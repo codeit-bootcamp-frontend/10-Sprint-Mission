3bc41224e3bf07a8e373b2c40922f294aff9e100
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-13 13:57:34 +0900

    feat: ProductPrice 커스텀 훅 추가

--- src/components/additem/ProductPrice.jsx
+++ src/components/additem/ProductPrice.jsx
@@ -1,29 +1,29 @@
 import styles from "./ProductPrice.module.css";
 import commonStyles from "./AddItemCommon.module.css";
-import { useState } from "react";
+import { useAddItemSharedData } from "./useAddItemSharedData";
 
 const ProductPrice = () => {
-  const [value, setValue] = useState('');
+  const { productPrice, setProductPrice } = useAddItemSharedData();
 
   const handleChange = (e) => {
     if(e.target.value === '') {
-      setValue('');
+      setProductPrice('');
       return;
     }
 
     const inputValue = Number(e.target.value.replace(/[^0-9]/g, '')).toLocaleString();
     
     if(inputValue === '') {
-      setValue('');
+      setProductPrice('');
       return;
     }
-    setValue(inputValue);
+    setProductPrice(inputValue);
   }
 
   return (
     <div className={commonStyles['common-container']}>
       <h5 className={commonStyles['common-product-title']}>판매가격</h5>
-      <input type="text" value={value} onChange={handleChange} className={styles['price-input']} placeholder="판매 가격을 입력해주세요" />
+      <input type="text" value={productPrice} onChange={handleChange} className={styles['price-input']} placeholder="판매 가격을 입력해주세요" />
     </div>
   );
 };

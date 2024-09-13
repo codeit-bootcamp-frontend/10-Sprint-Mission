cf58aa3e07f06493751659336cfa2d467512e931
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-13 13:52:55 +0900

    feat: ProductTitle 커스텀 훅 추가

--- src/components/additem/ProductTitle.jsx
+++ src/components/additem/ProductTitle.jsx
@@ -1,11 +1,18 @@
 import styles from "./ProductTitle.module.css";
 import commonStyles from "./AddItemCommon.module.css";
+import { useAddItemSharedData } from "./useAddItemSharedData";
 
 const ProductTitle = () => {
+  const { productTitle, setProductTitle } = useAddItemSharedData();
+
+  const handleProductTitleChange = (e) => {
+    setProductTitle(e.target.value);
+  }
+
   return (
     <div className={commonStyles['common-container']}>
       <h5 className={commonStyles['common-product-title']}>상품명</h5>
-      <input className={styles['product-title-input']} type="text" placeholder="상품명을 입력해주세요" />
+      <input className={styles['product-title-input']} value={productTitle} onChange={handleProductTitleChange} type="text" placeholder="상품명을 입력해주세요" />
     </div>
   );
 };

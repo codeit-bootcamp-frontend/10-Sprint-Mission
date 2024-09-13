379df674388cc7da591afcc7c6b4a8a751f49055
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-13 14:50:06 +0900

    feat: additem page 커스텀 훅 적용

--- src/pages/additem/AddItem.jsx
+++ src/pages/additem/AddItem.jsx
@@ -6,18 +6,32 @@ import ProductPrice from "components/additem/ProductPrice";
 import ProductTagList from "components/additem/ProductTagList";
 import ProductTitle from "components/additem/ProductTitle";
 import Header from "components/Header";
+import { useAddItemSharedData } from "components/additem/useAddItemSharedData";
 
 const AddItem = () => {
+  const { 
+    productImage, 
+    productTitle, 
+    productDescription, 
+    productPrice, 
+    productTags,
+    setProductImage,
+    setProductTitle,
+    setProductDescription,
+    setProductPrice,
+    setProductTags
+  } = useAddItemSharedData();
+
   return (
     <div>
       <Header/>
       <AddProduct/>
-      <AddProductImage/>
-      <ProductTitle/>
-      <ProductDescription/>
-      <ProductPrice/>
-      <AddProductTag/>
-      <ProductTagList/>
+      <AddProductImage productImage={productImage} setProductImage={setProductImage}/>
+      <ProductTitle productTitle={productTitle} setProductTitle={setProductTitle} />
+      <ProductDescription productDescription={productDescription} setProductDescription={setProductDescription}/>
+      <ProductPrice productPrice={productPrice} setProductPrice={setProductPrice}/>
+      <AddProductTag setProductTags={setProductTags}/>
+      <ProductTagList productTags={productTags} setProductTags={setProductTags}/>
     </div>
   );
 }

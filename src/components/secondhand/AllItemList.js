3192546e915beeefd4a622432b3c7c1cc7ecd4a9
jsh1147 <jsh1147@naver.com>
2024-09-13 18:19:30 +0900

    feat: 전체 상품 헤더 CSS 작성
    - 전체 상품 헤더를 ItemListHead로 분리
    - 피그마 디자인에 맞춰 CSS 구현

--- src/components/secondhand/AllItemList.js
+++ src/components/secondhand/AllItemList.js
@@ -1,6 +1,6 @@
-import { useState, useEffect, useCallback } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
+import ItemListHead from "./ItemListHead.js";
 import Item from "./Item";
 import PagenationBar from "./PagenationBar";
 
@@ -21,41 +21,13 @@ export default function AllItemList() {
   });
   const [isLoading, error, data] = useApi(getProducts, paramObj);
 
-  const handleInputEnter = useCallback((e) => {
-    if (e.key === "Enter")
-      setParamObj((prevObj) => ({
-        ...prevObj,
-        page: 1,
-        keyword: e.target.value,
-      }));
-  }, []);
-
-  const handleSelectChange = useCallback((e) => {
-    setParamObj((prevObj) => ({
-      ...prevObj,
-      page: 1,
-      orderBy: e.target.value,
-    }));
-  }, []);
-
   useEffect(() => {
     setParamObj((prevObj) => ({ ...prevObj, pageSize: pageSizeTable[media] }));
   }, [media]);
 
   return (
     <section className="allItemList">
-      <div className="allItemList__head">
-        <h2 className="allItemList__title">전체 상품</h2>
-        <input
-          onKeyUp={handleInputEnter}
-          placeholder="검색할 상품을 입력해 주세요"
-        ></input>
-        <Link to="/additem">상품 등록하기</Link>
-        <select onChange={handleSelectChange}>
-          <option value="recent">최신순</option>
-          <option value="favorite">좋아요순</option>
-        </select>
-      </div>
+      <ItemListHead setParamObj={setParamObj} />
       {!isLoading && !error && (
         <>
           <div className="allItemList__body">

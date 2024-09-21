4ddb038566f753519665c729c5db62a6ea6e5cad
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-21 23:23:07 +0900

    feat: Inquire 수정/삭제 기능 추가

--- src/components/items/productId/InquireItem.jsx
+++ src/components/items/productId/InquireItem.jsx
@@ -1,6 +1,13 @@
+import { useRef, useState } from 'react';
 import styles from './InquireItem.module.css';
+import { ReactComponent as EllipsisIcon } from 'assets/imgs/ic_ellipsis.svg';
 
 const InquireItem = ({content, userNickname, userImage ='assets/imgs/user_icon.svg', updateAt}) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [patchComment, setPatchComment] = useState(content);
+  const [isPatchClicked, setIsPatchClicked] = useState(false);
+  const textAreaRef = useRef(null);
+  const containerRef = useRef(null);
 
   const  timeDifferenceFromNow = (pastTime) => {
     const now = new Date();
@@ -23,9 +30,59 @@ const InquireItem = ({content, userNickname, userImage ='assets/imgs/user_icon.s
     }
   }
 
+  const handlePatch = () => {
+    setIsDropdownOpen(false);
+    setIsPatchClicked(true);
+  }
+
+  const handleDelete = () => {
+    setIsDropdownOpen(false);
+    containerRef.current.style.display = 'none';
+  }
+
+  const handlePatchComment = () => {
+    setIsPatchClicked(false);
+    setPatchComment(textAreaRef.current.value);
+  }
+
+  const handleCancelPatch = () => {
+    setIsPatchClicked(false);
+  }
+
   return (
-    <div className={styles['container']}>
-      <p className={styles['content']}>{content}</p>
+    <div ref={containerRef} className={styles['container']}>
+      <div className={styles['content-wrapper']}>
+        <div className={styles['content']}>
+        {isPatchClicked ? 
+        <div className={styles['patch-container']}>
+          <textarea
+            ref={textAreaRef}
+            className={styles['patch-text']}
+            placeholder='개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다.'
+            value={patchComment}
+            onChange={(e) => setPatchComment(e.target.value)}
+          />
+          <div className={styles['patch-content']}>
+            <span  className={styles['patch-cancel']} onClick={handleCancelPatch}>취소</span>
+            <button className={styles['patch-button']} onClick={handlePatchComment}>
+              수정완료
+            </button>
+          </div>
+        </div>
+        :
+        <>
+          {patchComment} 
+          <span onClick={()=>setIsDropdownOpen(!isDropdownOpen)}><EllipsisIcon /></span>
+        </>
+        }
+        </div>
+        {isDropdownOpen && (
+          <div className={styles['dropdown']}>
+            <span onClick={handlePatch} className={styles['dropdown-item']}>수정하기</span>
+            <span onClick={handleDelete} className={styles['dropdown-item']}>삭제하기</span>
+          </div>
+        )}
+      </div>
       <div className={styles['user-info']}>
         <img className={styles['user-image']} src={userImage} alt="user" />
         <div className={styles['user-info-content']}>

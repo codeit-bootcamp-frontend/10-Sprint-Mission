9cb2457d1c49f3031b25a546061cd0be77a5c74d
jsh1147 <jsh1147@naver.com>
2024-09-21 23:53:24 +0900

    feat: 삼풍 등록 페이지 기초 CSS 작성
    - 기초적인 CSS 적용

--- src/pages/addItemPage/AddItemFormSection.js
+++ src/pages/addItemPage/AddItemFormSection.js
@@ -1,31 +1,48 @@
+import "./AddItemFormSection.css";
+
 export default function AddItemFormSection() {
   return (
-    <section className="addItemFormSection">
-      <form>
-        <fieldset>
-          <legend>
-            <h1>상품 등록하기</h1>
-          </legend>
-          <button>등록</button>
+    <section className="addItemForm">
+      <form className="form">
+        <fieldset className="form__head">
+          <h1 className="form__title">상품 등록하기</h1>
+          <button className="form__submitButton">등록</button>
         </fieldset>
-        <fieldset>
-          <label>상품 이미지</label>
+        <fieldset className="form__body">
+          <label className="form__label">상품 이미지</label>
           <div>
-            <input type="file" accept="image/*"></input>
-            <button type="button">이미지 등록</button>
+            <input hidden type="file" accept="image/*"></input>
+            <button className="form__imageInput" type="button">
+              이미지 등록
+            </button>
             <div>
               <img src="" alt="" />
               <button type="button" />
             </div>
           </div>
-          <label>상품명</label>
-          <input type="text" placeholder="상품명을 입력해 주세요"></input>
-          <label>상품 소개</label>
-          <textarea placeholder="상품 소개를 입력해 주세요"></textarea>
-          <label>판매 가격</label>
-          <input type="text" placeholder="판매 가격을 입력해 주세요"></input>
-          <label>태그</label>
-          <input type="text" placeholder="태그를 입력해 주세요"></input>
+          <label className="form__label">상품명</label>
+          <input
+            className="form__input"
+            type="text"
+            placeholder="상품명을 입력해 주세요"
+          ></input>
+          <label className="form__label">상품 소개</label>
+          <textarea
+            className="form__input"
+            placeholder="상품 소개를 입력해 주세요"
+          ></textarea>
+          <label className="form__label">판매 가격</label>
+          <input
+            className="form__input"
+            type="text"
+            placeholder="판매 가격을 입력해 주세요"
+          ></input>
+          <label className="form__label">태그</label>
+          <input
+            className="form__input"
+            type="text"
+            placeholder="태그를 입력해 주세요"
+          ></input>
           {[1].map((tag) => (
             <div key={tag}>
               <span>#</span>

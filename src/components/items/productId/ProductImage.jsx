f77b010bb70e052f0f4c6b526106129d37f07336
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-21 19:10:56 +0900

    feat: ProductImage 컴포넌트 jsx css 작성

--- src/components/items/productId/ProductImage.jsx
+++ src/components/items/productId/ProductImage.jsx
@@ -1,7 +1,9 @@
-const ProductImage = () => {
+import styles from './ProductImage.module.css';
+
+const ProductImage = ({productImage}) => {
   return (
     <div>
-      
+      <img className={styles['container']} src={productImage} alt="상품 이미지" />
     </div>
   );
 };

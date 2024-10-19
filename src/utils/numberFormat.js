e340fd3a0dfca9e33fc413388bb0370222aa24bf
heejin <dlwlrgkwk82@gmail.com>
2024-10-19 18:42:20 +0900

    feat: Add typescrips on js files

--- src/utils/numberFormat.js
+++
@@ -1,3 +0,0 @@
-export function formatNumberWithCommas(number) {
-  return number.toLocaleString();
-}
--- src/utils/paging.ts
+++ src/utils/paging.ts
@@ -1,18 +1,18 @@
-function getPageSize(width, sizes) {
+function getPageSize(width: number, sizes: number[]): number {
   if (width <= 767) return sizes[0];
   if (width <= 1199) return sizes[1];
   return sizes[2];
 }
 
-export function getBestItemsPageSize() {
+export function getBestItemsPageSize(): number {
   const width = window.innerWidth;
   return getPageSize(width, [1, 2, 4]);
 }
 
-export function getAllItemsPageSize() {
+export function getAllItemsPageSize(): number {
   const width = window.innerWidth;
   return getPageSize(width, [4, 6, 10]);
 }
 
-export const LIMIT = 10;
-export const DISPLAY_PAGINATION_NUMBER = 5;
+export const LIMIT = 10 as const;
+export const DISPLAY_PAGINATION_NUMBER = 5 as const;

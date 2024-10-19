90de1e71178005cec2e29651b0aa7197d9a6b2ed
heejin <dlwlrgkwk82@gmail.com>
2024-10-19 19:20:15 +0900

    feat: Add typescripts on common components

---
+++ src/components/PrimaryButton.tsx
@@ -0,0 +1,18 @@
+import './PrimaryButton.css';
+
+interface PrimaryButtonProps {
+  text: string;
+  type: 'button' | 'submit' | 'reset';
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled: boolean;
+}
+
+function PrimaryButton({ text, type = 'button', onClick, disabled = false }: PrimaryButtonProps) {
+  return (
+    <button type={type} className="PrimaryButton" onClick={onClick} disabled={disabled}>
+      {text}
+    </button>
+  );
+}
+
+export default PrimaryButton;
---
+++ src/components/PrimaryRoundButton.tsx
@@ -0,0 +1,23 @@
+import './PrimaryRoundButton.css';
+
+interface PrimaryRoundButtonProps {
+  children: React.ReactNode;
+  type: 'button' | 'submit' | 'reset';
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled: boolean;
+}
+
+function PrimaryRoundButton({
+  children,
+  type = 'button',
+  onClick,
+  disabled = false,
+}: PrimaryRoundButtonProps) {
+  return (
+    <button type={type} className="PrimaryRoundButton" onClick={onClick} disabled={disabled}>
+      {children}
+    </button>
+  );
+}
+
+export default PrimaryRoundButton;

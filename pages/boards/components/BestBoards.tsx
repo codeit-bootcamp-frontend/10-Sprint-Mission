455b5b37875a74ec49458e0e462eeb7e52cc5935
heejin <dlwlrgkwk82@gmail.com>
2024-10-26 03:54:01 +0900

    feat: Add bestBoards title

--- pages/boards/components/BestBoards.tsx
+++ pages/boards/components/BestBoards.tsx
@@ -3,10 +3,13 @@ import styles from './BestBoards.module.css';
 
 export default function BestBoards() {
   return (
-    <div className={styles.bestBoards}>
-      <BestBoardCard />
-      <BestBoardCard />
-      <BestBoardCard />
-    </div>
+    <section>
+      <h3 className={styles.title}>베스트 게시글</h3>
+      <div className={styles.bestBoards}>
+        <BestBoardCard />
+        <BestBoardCard />
+        <BestBoardCard />
+      </div>
+    </section>
   );
 }

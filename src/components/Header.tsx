657ccd650b7798b5f3455ac0bba46d42f7f955b3
heejin <dlwlrgkwk82@gmail.com>
2024-11-02 00:45:50 +0900

    refactor: code review

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,32 @@ import Link from 'next/link';
 import styles from './Header.module.css';
 import Image from 'next/image';
 import avatarSvg from '@/src/assets/avatar.svg';
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <header className={styles.header}>
-      <div className={`${styles.headerContainer}`}>
+      <div className={styles.headerContainer}>
         <Link
           href={'/'}
           className={styles.logoWrapper}
           aria-label="홈으로 이동"
         />
         <nav className={styles.nav}>
-          <Link href={'/boards'}>자유게시판</Link>
-          <Link href={'/'}>중고마켓</Link>
+          <Link
+            href={'/boards'}
+            className={`link ${pathname === '/boards' ? styles.active : ''}`}
+          >
+            자유게시판
+          </Link>
+          <Link
+            href={'/'}
+            className={`link ${pathname === '/' ? styles.active : ''}`}
+          >
+            중고마켓
+          </Link>
         </nav>
         <div className={styles.avatarWrapper}>
           <Image fill src={avatarSvg} alt="avatar" />

93e2622b27e3bbd3f82969fb58c09c62c036bad2
jsh1147 <jsh1147@naver.com>
2024-11-09 23:56:29 +0900

    feat: 로그인/회원가입 API 동작 적용
    - 로그인/회원가입 폼에 API 연결

--- src/components/pages/account/SignUpForm.tsx
+++ src/components/pages/account/SignUpForm.tsx
@@ -1,10 +1,47 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { useRouter } from "next/router";
 import Image from "next/image";
+import { PostSignUpParams, PostSignUpRes } from "@/apis/apis.type";
+import { useQuery } from "@/hooks/useQuery";
+import { postSignUp } from "@/apis/apis";
 import closedEye from "#/icons/eye_close.svg";
 import styles from "./SignUpForm.module.css";
 
 export default function SignUpForm() {
+  const [formData, setFormData] = useState<PostSignUpParams>({
+    email: "",
+    nickname: "",
+    password: "",
+    passwordConfirmation: "",
+  });
+  const { error, data, query } = useQuery<PostSignUpParams, PostSignUpRes>(
+    postSignUp
+  );
+  const { replace } = useRouter();
+
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    query(formData);
+
+    if (error || !data) {
+      alert("회원가입에 실패했습니다.\n다시 시도해 주세요!");
+      return;
+    }
+
+    alert("회원가입에 성공했습니다.\n로그인 후 이용해 주세요!");
+    replace("/login");
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevData) => {
+      const nextData = { ...prevData };
+      nextData[e.target.name as keyof PostSignUpParams] = e.target.value;
+      return nextData;
+    });
+  };
+
   return (
-    <form className={styles.form} method="post">
+    <form className={styles.form} method="post" onSubmit={handleFormSubmit}>
       <div className={styles.formField}>
         <label className={styles.label} htmlFor="emailInput">
           이메일
@@ -15,6 +52,8 @@ export default function SignUpForm() {
           name="email"
           type="email"
           placeholder="이메일을 입력해 주세요"
+          value={formData.email}
+          onChange={handleInputChange}
         />
         <span className={styles.error}></span>
       </div>
@@ -28,6 +67,8 @@ export default function SignUpForm() {
           name="nickname"
           type="text"
           placeholder="닉네임을 입력해 주세요"
+          value={formData.nickname}
+          onChange={handleInputChange}
         />
         <span className={styles.error}></span>
       </div>
@@ -41,6 +82,8 @@ export default function SignUpForm() {
           name="password"
           type="password"
           placeholder="비밀번호를 입력해 주세요"
+          value={formData.password}
+          onChange={handleInputChange}
         />
         <button className={styles.toggleButton} type="button">
           <Image src={closedEye} alt="비밀번호 보기" />
@@ -54,9 +97,11 @@ export default function SignUpForm() {
         <input
           className={styles.input}
           id="passwordConfirmInput"
-          name="passwordConfirm"
+          name="passwordConfirmation"
           type="password"
           placeholder="비밀번호를 다시 한 번 입력해 주세요"
+          value={formData.passwordConfirmation}
+          onChange={handleInputChange}
         />
         <button className={styles.toggleButton} type="button">
           <Image src={closedEye} alt="비밀번호 보기" />

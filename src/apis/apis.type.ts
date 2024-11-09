9b7c5e3c5575550ce4aae864e8ef8b406530a793
jsh1147 <jsh1147@naver.com>
2024-11-09 22:22:50 +0900

    feat: 로그인/회원가입 API 구현
    - fetch 함수를 활용한 로그인/회원가입 API 구현

--- src/apis/apis.type.ts
+++ src/apis/apis.type.ts
@@ -27,3 +27,30 @@ export interface ArticleProps {
 }
 export type GetArticlesParams = GetListParams<ArticleOrderType>;
 export type GetArticlesRes = GetListRes<ArticleProps>;
+
+interface UserProps {
+  user: {
+    id: number;
+    nickname: string;
+    image: string | null;
+    createdAt: string;
+    updatedAt: string;
+    email: string;
+  };
+  accessToken: string;
+  refreshToken: string;
+}
+
+export interface PostSignUpParams {
+  email: string;
+  nickname: string;
+  password: string;
+  passwordConfirmation: string;
+}
+export type PostSignUpRes = UserProps;
+
+export interface PostLogInParams {
+  email: string;
+  password: string;
+}
+export type PostLogInRes = UserProps;

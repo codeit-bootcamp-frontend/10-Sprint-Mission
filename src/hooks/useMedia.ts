b536021899a87f82f85e7177638a503b5b05ec2a
jsh1147 <jsh1147@naver.com>
2024-11-09 18:04:19 +0900

    refactor: 미디어 훅 위치 조정
    - 미디어 컨텍스트 파일 내부로 이동

--- src/hooks/useMedia.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { MediaContext } from "@/store/MediaContext";
-
-export function useMedia() {
-  const media = useContext(MediaContext);
-  return media;
-}
